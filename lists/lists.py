0a33e7f9ad4678ff54259af55a7489f2b5812eac
Chera <chera1904@mail.ru>
2023-04-26 13:56:51 +0600

    Test lists

--- lists/lists.py
+++ lists/lists.py
@@ -4,20 +4,44 @@ class ListExercise:
         """
         Заменить все положительные элементы целочисленного списка на максимальное значение
         элементов списка.
-
         :param input_list: Исходный список
         :return: Список с замененными элементами
         """
-        pass
+        if input_list:
+            max_element = sorted(input_list)[-1]
+            return list(map(lambda x: max_element if x > 0 else x, input_list))
+        else:
+            return []
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
         """
         Реализовать двоичный поиск
         Функция должна возвращать индекс элемента
-
         :param input_list: Исходный список
         :param query: Искомый элемент
         :return: Номер элемента
         """
-        pass
+        if query not in input_list:
+            return -1
+        low = 0
+        high = len(input_list)
+        return ListExercise.binary_search(input_list, low, high, query)
+
+    @staticmethod
+    def binary_search(input_list: list[int], low, high, query):
+        """
+        Реализация функции бинарного поиска требуемого индекса по переданному списку
+        :param input_list: Исходный список
+        :param low: Нижнее значение
+        :param high: Верхнее значение
+        :param query Искомый элемент
+        :return: Искомый номер элемента
+        """
+        mid = (low + high) // 2
+        if input_list[mid] != query:
+            if query < input_list[mid]:
+                return ListExercise.binary_search(input_list, low, mid - 1, query)
+            return ListExercise.binary_search(input_list, mid + 1, high, query)
+        else:
+            return mid
